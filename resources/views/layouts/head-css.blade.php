233281f52ce6e2d1674261166ca74792a8af4d24
JavierSA <79856488+JavierSA-dev@users.noreply.github.com>
2026-03-21 18:46:53 +0100

    feat: Agregar soporte para SweetAlert2 en las vistas

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -1,5 +1,7 @@
 @yield('css')
 
+<!-- SweetAlert2 -->
+<link href="{{ asset('build/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- Bootstrap Css -->
 <link href="{{ asset('build/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
